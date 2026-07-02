47c8306114333237e8063ef757f2e088a359a962
SHEETAL_SUBBANNAVAR <sheetalssubbu@gmail.com>
2026-07-02 18:27:20 +0530

    Time: 0 ms (100%), Space: 43.7 MB (88.75%) - LeetHub

--- 3798-largest-even-number/3798-largest-even-number.java
+++ 3798-largest-even-number/3798-largest-even-number.java
@@ -1,7 +1,7 @@
 class Solution {
     public String largestEven(String s) {
         for(int i = s.length()-1; i>=0;i--){
-            if(s.charAt(i)%2 == 0) return s.substring(0,i+1);
+            if((s.charAt(i)- '0')%2 == 0) return s.substring(0,i+1);
         }
         return "";
     }
