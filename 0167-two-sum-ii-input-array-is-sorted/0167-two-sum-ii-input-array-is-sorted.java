4c0b29a3d76c4c250d1b2728f49e670f77e45a00
SHEETAL SUBBANNAVAR <sheetalssubbu@gmail.com>
2026-09-19 10:12:29 +0530

    Time: 2 ms (96.01%), Space: 48.2 MB (79.45%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.java
@@ -3,10 +3,11 @@ class Solution {
         int low = 0;
         int high = numbers.length-1;
         while (low < high) {
-            if (numbers[low] + numbers[high] == target) {
+            int sum = numbers[low] + numbers[high];
+            if (sum == target) {
                 return new int[]{ low + 1, high + 1 };
                 
-            } else if (numbers[low] + numbers[high] > target)
+            } else if (sum > target)
                 high--;
             else
                 low++;
